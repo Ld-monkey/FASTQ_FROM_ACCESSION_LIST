fed2885878d7e6862b494731a7756eba64d8242b
lfourteau91@gmail.com <lfourteau91@gmail.com>
2020-03-09 17:52:13 +0100

    Add program and README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# FASTQ_FROM_ACCESSION_LIST
-Retrieving sequences of interest from the accession list of a ncbi database.
+# FASTQ FROM ACCESSION LIST
+Get all sequences of interest from the accession list of a ncbi database in one fastq file.
+
+# Using the script
+```bash
+python get_database_from_accession_list.py -id only_five_accesssion_list.seq -o result
+```
+* The -id parameter is the path to the accesssion list file.
+* The -o parameter is the name of the output file.
+
+# Dependencies
+* [BioPython](https://biopython.org/)
+* Accession list (sequence.seq) from your database.
---
+++ get_database_from_accession_list.py
@@ -0,0 +1,87 @@
+'''
+Program to download all fasta sequences of database from the accesion list.
+'''
+
+# coding: utf-8
+
+import os.path
+import sys
+import argparse
+from Bio import Entrez
+
+def check_is_file_exist(file):
+    """ Check if the file exists. """
+    if os.path.isfile(file):
+        print("File exist")
+    else:
+        print("File not exist")
+        sys.exit(1)
+
+def get_id_accession_list(file):
+    """ Return a list of ID from accession list. """
+    with open(file, "r") as accession_file:
+        id_list = accession_file.read().splitlines()
+    return id_list
+
+def create_fastq_from_accession_list(basename_output_fastq, id_accession_list):
+    """ Create a fastq file from the id list of accesion list file. """
+
+    # Get the extention of basename_output_fastq.
+    file_name, extention = os.path.splitext(basename_output_fastq)
+
+    # Verified is the extention of basename_output_fastq is a fastq.
+    if extention is None or extention != ".fastq":
+        basename_output_fastq = file_name+".fastq"
+
+    # Add mail for ncbi service.
+    Entrez.email = "test@exemple.org"
+
+    # Create the fastq file.
+    try:
+        with open(basename_output_fastq, "w") as fasta_file:
+            for id_fastq in id_accession_list:
+                handle = Entrez.efetch(db="nucleotide",
+                                       id=id_fastq,
+                                       rettype="fasta",
+                                       retmode="text")
+                # Report of fasta.
+                report = handle.read()
+
+                # Display report.
+                print(report)
+
+                # Write the fasta file.
+                fasta_file.write(report)
+
+            # Close the handle.
+            handle.close()
+    except Exception as e:
+        print("Error in writting fasta file")
+
+def arguments():
+    """ Method for define all arguments of program """
+    parser = argparse.ArgumentParser(description="Get database from accession list")
+    parser.add_argument("-id",
+                        help="Accession list file (.seq)",
+                        type=str)
+    parser.add_argument("-o",
+                        help="Basename of output fastq file.",
+                        type=str)
+    args = parser.parse_args()
+    return args.id, args.o
+
+if __name__ == "__main__":
+    print("Download database and all fasta files with the accession list.")
+
+    # Get the first argument with accession list file and name of output fasta file.
+    ACCESSION_LIST_FILE, BASENAME_OUTPUT_FASTA = arguments()
+
+    # Check if accesion list exist.
+    check_is_file_exist(ACCESSION_LIST_FILE)
+
+    # Recover all id from accesion file.
+    ID_LIST = list()
+    ID_LIST = get_id_accession_list(ACCESSION_LIST_FILE)
+
+    # Create a fastq file from accession list.
+    create_fastq_from_accession_list(BASENAME_OUTPUT_FASTA, ID_LIST)
